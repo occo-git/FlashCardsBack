aba9962c5ea3d8221a2486724c31d9cce9f76dcd
occo-git <129328663+occo-git@users.noreply.github.com>
2025-11-26 02:53:05 +0400

    polly retry send email

--- Infrastructure/Services/EmailSender/EmailSender.cs
+++ Infrastructure/Services/EmailSender/EmailSender.cs
@@ -1,14 +1,22 @@
 ﻿using Application.Abstractions.Services;
+using MailKit.Net.Smtp;
 using MailKit.Security;
-using MimeKit;
 using Microsoft.Extensions.Options;
+using MimeKit;
+using Polly;
+using Polly.Retry;
 using Shared.Configuration;
+using System.ComponentModel;
+using System.Net.Mail;
+using System.Net.Sockets;
 
 namespace Infrastructure.Services.EmailSender
 {
     public class EmailSender : IEmailSender
     {
         private readonly SmtpOptions _smtpOptions;
+        private readonly AsyncRetryPolicy _retryPolicy;
+        private const int MaxRetryAttempts = 3;
 
         public EmailSender(IOptions<SmtpOptions> smtpOptions)
         {
@@ -16,21 +24,35 @@ namespace Infrastructure.Services.EmailSender
             ArgumentNullException.ThrowIfNull(smtpOptions.Value, nameof(smtpOptions.Value));
 
             _smtpOptions = smtpOptions.Value;
+
+            _retryPolicy = Policy
+                .Handle<SmtpCommandException>() // MailKit specific exception
+                .Or<SmtpProtocolException>()    // MailKit specific exception
+                .Or<SocketException>()          // Network issues, example
+                .Or<SmtpException>()         // General SMTP exception
+                .WaitAndRetryAsync(MaxRetryAttempts, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
         }
 
         public async Task SendEmailAsync(string toEmail, string subject, string htmlMessage)
         {
-            var mimeMessage = new MimeMessage();
-            mimeMessage.From.Add(MailboxAddress.Parse(_smtpOptions.From));
-            mimeMessage.To.Add(MailboxAddress.Parse(toEmail));
-            mimeMessage.Subject = subject;
-            mimeMessage.Body = new TextPart("html") { Text = htmlMessage };
+            int retryAttempt = 0;
+            await _retryPolicy.ExecuteAsync(async () =>
+            {
+                retryAttempt++;
+                Console.WriteLine($"EmailSender: Try {retryAttempt}/{MaxRetryAttempts} to send email to {toEmail}");
 
-            using var client = new MailKit.Net.Smtp.SmtpClient();
-            await client.ConnectAsync(_smtpOptions.Host, _smtpOptions.Port, SecureSocketOptions.StartTls);
-            await client.AuthenticateAsync(_smtpOptions.Account, _smtpOptions.Password);
-            await client.SendAsync(mimeMessage);
-            await client.DisconnectAsync(true);
+                var mimeMessage = new MimeMessage();
+                mimeMessage.From.Add(MailboxAddress.Parse(_smtpOptions.From));
+                mimeMessage.To.Add(MailboxAddress.Parse(toEmail));
+                mimeMessage.Subject = subject;
+                mimeMessage.Body = new TextPart("html") { Text = htmlMessage };
+
+                using var client = new MailKit.Net.Smtp.SmtpClient();
+                await client.ConnectAsync(_smtpOptions.Host, _smtpOptions.Port, SecureSocketOptions.StartTls);
+                await client.AuthenticateAsync(_smtpOptions.Account, _smtpOptions.Password);
+                await client.SendAsync(mimeMessage);
+                await client.DisconnectAsync(true);
+            });
         }
     }
 }
